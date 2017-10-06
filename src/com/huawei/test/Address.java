e9b9d25fd7463dd1a2bd81d2413f68f80e0984b0
manjeeetsingh <roy.2015@gmail.com>
2017-10-06 09:23:32 +0530

    Initial commit

---
+++ src/com/huawei/test/Address.java
@@ -0,0 +1,42 @@
+package com.huawei.test;
+
+public class Address {
+	private String streetName;
+	private String streetNumber;
+	private String buildingName;
+	private String floorNumber;
+	private String flatNumber;
+	public String getStreetName() {
+		return streetName;
+	}
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+	public String getStreetNumber() {
+		return streetNumber;
+	}
+	public void setStreetNumber(String streetNumber) {
+		this.streetNumber = streetNumber;
+	}
+	public String getBuildingName() {
+		return buildingName;
+	}
+	public void setBuildingName(String buildingName) {
+		this.buildingName = buildingName;
+	}
+	public String getFloorNumber() {
+		return floorNumber;
+	}
+	public void setFloorNumber(String floorNumber) {
+		this.floorNumber = floorNumber;
+	}
+	public String getFlatNumber() {
+		return flatNumber;
+	}
+	public void setFlatNumber(String flatNumber) {
+		this.flatNumber = flatNumber;
+	}
+
+}
+
+
---
+++ src/com/huawei/test/Employee.java
@@ -0,0 +1,29 @@
+package com.huawei.test;
+
+public class Employee {
+	
+	String name;
+	String jobProfile;
+	Address address ;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getJobProfile() {
+		return jobProfile;
+	}
+	public void setJobProfile(String jobProfile) {
+		this.jobProfile = jobProfile;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	
+	
+}
